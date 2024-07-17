8ae59565ddb1b1095905ae1d9fb97489e0d089f1
Mikey-Xt-30 <mikeyxt30@gmail.com>
2024-07-17 17:26:08 +0530

    commit first change

--- gitProj_01/src/p1/Arthematic.java
+++ gitProj_01/src/p1/Arthematic.java
@@ -1,12 +1,18 @@
 package p1;
 
+import java.util.Scanner;
+
 public class Arthematic 
 {
 	public static void main(String[]args)
 	{
-		int a =10;
-		int b = 10;
-		System.out.println("Sum of "+a+b);
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter value a:");
+		int a = s.nextInt();
+		System.out.println("Enter value b:");
+		int b = s.nextInt();
+	      int c = a+b;
+		System.out.println("Sum of "+c);
 	}
 
 }
